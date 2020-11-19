82a8904d59bb3089974fd85ed65f4f874fb43982
Vladimir Gorohov <v.g.1990@icloud.com>
2020-11-19 21:15:49 +0500

    task-3

--- task-3/main.js
+++ task-3/main.js
@@ -1,6 +1,7 @@
-var name = prompt('enter name');
-var age = prompt('enter age');
+var num1 = +prompt('enter number');
+var num2 = +prompt('enter number');
+var num3 = +prompt('enter number');
 
-var str = `name: ${name} age: ${age}`;
+var sum = num1 + num2 + num3;
 
-alert(str);
+alert(sum);
