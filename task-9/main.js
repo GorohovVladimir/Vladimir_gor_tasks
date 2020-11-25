f6f5cf00c860aaacc01d6cb773874982e4368000
Vladimir Gorohov <v.g.1990@icloud.com>
2020-11-25 21:42:32 +0500

    task-9

---
+++ task-9/main.js
@@ -0,0 +1,30 @@
+
+function arrNumber() {
+    var arr = ['234', '176', '502', '306', '412'];
+    return arr;
+}
+
+var myJoin = arrNumber().join('');
+
+function myArr()  {
+
+    var str = '';
+
+    for(var char of myJoin){
+        if(char % 2 === 0){
+            str += `${char} `;
+        }
+        else {
+            str += `${char}`;
+        }
+    }
+    return str;
+}
+
+var resFunc = myArr(myJoin);
+
+var result = resFunc.split(' ');
+
+console.log(result);
+
+
