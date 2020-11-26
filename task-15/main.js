d92d21520d85c7ed3ddaf81769d79a46dc5f044b
Vladimir Gorohov <v.g.1990@icloud.com>
2020-11-26 18:47:27 +0500

    task-15

---
+++ task-15/main.js
@@ -0,0 +1,27 @@
+
+function object() {
+var userName = 'Alex';
+var userAge = 18;
+var userCity = 'New York';
+
+var myObject = {};
+
+myObject['name'] = userName;
+myObject['age'] = userAge;
+myObject['city'] = userCity;
+
+return console.log(myObject);
+
+}
+
+object();
+
+
+
+
+
+
+
+
+
+
