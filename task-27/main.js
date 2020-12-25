1e977455324bb1ac8fe5a61f84d968109dfe1cfa
Vladimir Gorohov <v.g.1990@icloud.com>
2020-12-25 22:19:51 +0500

    task-27

---
+++ task-27/main.js
@@ -0,0 +1,47 @@
+
+var enterText = prompt('Enter list words and pattern wrapper');
+
+  var textToArrey = enterText.split('*')
+
+  var textNames = textToArrey[0];
+  var pattern = textToArrey[1];
+
+  var namesToArrey = textNames.split(',');
+
+  var res = '';
+
+  for(i = 0; i < namesToArrey.length; i++){
+    for(var char of pattern){
+      if(char === '$'){
+        res += `${namesToArrey[i]}`
+      }else {
+        res += char
+      }
+    }
+  }
+
+  var container = document.querySelector('#container__names');
+
+  container.innerHTML = res;
+
+  
+
+ 
+
+
+      
+
+
+  
+
+ 
+
+
+
+
+
+
+
+
+
+
